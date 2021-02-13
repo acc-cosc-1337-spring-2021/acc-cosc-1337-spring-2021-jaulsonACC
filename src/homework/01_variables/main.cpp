e97bae44fd4fdc6ccbf8aa34cf463acfd3700cf4
Github Username <jonathan.aulson@g.austincc.edu>
2021-02-12 18:42:33 -0600

    Homework 2

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,15 +1,39 @@
 ﻿//write include statements
+#include "variables.h"
+#include<iostream>
 
 
 //write namespace using statement for cout
+using std::cout;
+using std::cin;
 
 
-/*
-Call multiply_numbers with 10 and 10 parameter values and display function result
-*/
 int main()
 {
-	
+	double meal_amount;
+	double tip_rate;
+	double tip_amount;
+	double tax_amount;
+	double total;
+
+	cout<<"Enter the meal amount in dollars and cents: ";
+	cin>>meal_amount;
+
+	tax_amount = get_sales_tax(meal_amount);
+
+	cout<<"Enter the tip amount in decimal: ";
+	cin>>tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = tip_amount + tax_amount + meal_amount;
+
+	cout<<"Meal Amount:\t$"<<meal_amount<<"\n";
+	cout<<"Tax Amount:\t$"<<tax_amount<<"\n";
+	cout<<"Tip Amount:\t$"<<tip_amount<<"\n";
+	cout<<"---------------------------\n";
+	cout<<"Total:\t$"<<total<<"\n";
+
 
 	return 0;
 }
--- src/homework/01_variables/variables.cpp
+++ src/homework/01_variables/variables.cpp
@@ -12,3 +12,13 @@ int add_numbers(int num1, int num2)
 
 
 
+double get_sales_tax(double meal_amount)
+{
+	double tax_rate = 0.0675;
+	return tax_rate * meal_amount;
+}
+
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	return tip_rate * meal_amount;
+}
